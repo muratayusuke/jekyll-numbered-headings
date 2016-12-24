99a8508b7d82f851b87a71a7fe1707ed08a8d674
muratayusuke <info@muratayusuke.com>
2016-12-24 17:49:10 +0900

    Bugfix

--- lib/jekyll-numbered-headings.rb
+++ lib/jekyll-numbered-headings.rb
@@ -1,7 +1,2 @@
 require 'jekyll/numbered_headings/version'
-require 'jekyll/_plugins/numbered_plugins'
-
-module Jekyll
-  module NumberedHeadings
-  end
-end
+require 'jekyll/_plugins/numbered_headings'
