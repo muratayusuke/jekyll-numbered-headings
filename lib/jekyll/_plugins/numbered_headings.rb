c54b8195ea500d25eb815cb36bef97e11c0fc252
Randall Pittman <randall.pittman@oregonstate.edu>
2022-07-06 14:38:47 -0700

    Enable with front matter flag instead of special headings

--- lib/jekyll/_plugins/numbered_headings.rb
+++ lib/jekyll/_plugins/numbered_headings.rb
@@ -3,11 +3,13 @@ Jekyll::Hooks.register [:pages, :posts, :documents], :pre_render do |article|
   levels = Array.new(max_level, 0)
   in_code_block = false
 
+  return unless article.data['numbered-headings'] == true
+
   converted_lines = article.content.split("\n").map do |line|
     in_code_block = !in_code_block if line.match(/^```/)
     next line if in_code_block
 
-    matched = line.match(/^(#+)1\. (.+)/)
+    matched = line.match(/^(#+) (.+)/)
     next line unless matched
 
     heading = matched[1]
